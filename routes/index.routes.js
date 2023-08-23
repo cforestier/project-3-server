865b5208d7bfa6349bd2bd0abe2e0a5d5b543385
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-08-23 16:31:56 +0200

    upload image route done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const fileUploader = require('../config/cloudinary.config')
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.post('/upload', fileUploader.single("imageUrl"), (req, res, next) => {
+  if (!req.file) {
+    return res.status(404).json({ message: "no file uploaded" })
+  }
+
+  return res.json({ fileUrl: req.file.path })
+})
 
 module.exports = router;
