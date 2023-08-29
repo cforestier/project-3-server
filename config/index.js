20f5f6f12f6f77715159fc5d023c2d98ada7e5f2
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-08-29 22:41:39 +0200

    deploy

--- config/index.js
+++ config/index.js
@@ -22,7 +22,11 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(cors());
+  app.use(
+    cors({
+      origin: '*',
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
