93877716a893effc5f142812aa547430148da7fb
Raphaël Malet <malet.raphael@gmail.com>
2023-09-01 14:40:05 +0200

    update of user password possible on modal

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 router.get("/:userId", (req, res, next) => {
   // gets the userId from params
@@ -36,24 +38,74 @@ router.get("/:userId", (req, res, next) => {
 
 router.put("/edit/:userId", isAuthenticated, (req, res, next) => {
   const user = req.payload;
-  const { username, image } = req.body;
-  console.log(req.body)
+  const { username, password } = req.body;
+  if (username.length < 5)
+    return res
+      .status(200)
+      .json({ message: "username can not be less than 5 characters" });
 
-  if (username.length < 5) return res.status(200).json({message: "username can not be less than 5 characters"})
-
-  User.findOne({ username }).then((foundUser) => {
-    if (foundUser) {
-      return res.json({ message: "Username taken" });
-    } else {
-      User.findByIdAndUpdate(user._id, { username, image }, { new: true }).then(
-        (foundUser) => {
-          const { username, image } = foundUser;
-          // new user object that we will return to front end
-          return res.status(200).json({ username, image });
-        }
-      );
+  if (password && username) {
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      });
+      return;
     }
-  });
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+      if (err) {
+        return res
+          .statusMessage(500)
+          .json({ message: "Password hashing error" });
+      }
+      User.findByIdAndUpdate(
+        user._id,
+        { username, password: hashedPassword },
+        { new: true }
+      ).then((foundUser) => {
+        const { username } = foundUser;
+        return res.status(200).json({ username });
+      });
+    });
+  } else if (password) {
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      });
+      return;
+    }
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) {
+        return res.status(500).json({ message: "Password hashing error" });
+      }
+
+      User.findByIdAndUpdate(
+        user._id,
+        { password: hashedPassword },
+        { new: true }
+      ).then((foundUser) => {
+        const { username } = foundUser;
+        return res.status(200).json({ username });
+      });
+    });
+  } else if (username) {
+    User.findOne({ username }).then((foundUser) => {
+      if (foundUser) {
+        return res.json({ message: "Username taken" });
+      } else {
+        User.findByIdAndUpdate(user._id, { username }, { new: true }).then(
+          (foundUser) => {
+            const { username } = foundUser;
+            // new user object that we will return to front end
+            return res.status(200).json({ username });
+          }
+        );
+      }
+    });
+  }
 });
 
 router.put("/edit/:userId/image", isAuthenticated, (req, res, next) => {
