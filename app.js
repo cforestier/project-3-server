01c3fe3933533331724ad8ee62b654d9127b322d
Raphaël Malet <malet.raphael@gmail.com>
2023-09-07 20:42:00 +0200

    creating routes for orders

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use("/chat", chatRoutes);
 const messageRoutes = require("./routes/message.routes");
 app.use("/messages", messageRoutes);
 
+const ordersRoutes = require("./routes/orders.routes");
+app.use("/messages", ordersRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
